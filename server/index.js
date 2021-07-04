b31856b41d09808b1393df41ec274ad91fc56f41
Minakrv <mnkrv@yahoo.com>
2021-07-05 00:30:00 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,11 +3,10 @@ const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(cors());
-
+app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -15,13 +14,7 @@ const db = mysql.createConnection({
     database: 'employeeSystem'
   });
   
-  db.connect(function(err) {
-    if (err) throw err;
-    db.query("SELECT * FROM empolyee", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  });
+  
 
 app.post('/create', (req,res)=>{
     const name = req.body.name;
@@ -29,7 +22,7 @@ app.post('/create', (req,res)=>{
     const email = req.body.email;
     const position = req.body.position;
 
-    db.Query('INSERT INTO employee (name, age, email, position) VALUES (?,?,?,?)', 
+    db.query('INSERT INTO employee (name, age, email, position) VALUES (?,?,?,?)', 
     [name, age, email, position], (err, result) =>{
         if(err){
             console.log(err);
@@ -38,10 +31,20 @@ app.post('/create', (req,res)=>{
         }
     }
     );
+});
+
+app.get('/employees', (req,re) => {
+    db.query('SELECT * employee', (err,result) =>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
 })
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
 
-  module.exports = db;
+  
