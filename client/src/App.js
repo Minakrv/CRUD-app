33924103d1405cb94ab9f7c76c4d0803e045e9f2
Minakrv <mnkrv@yahoo.com>
2021-07-04 03:23:27 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
 import { useState } from "react";
 import Axios from 'axios';
-import axios from "axios";
+
 function App() {
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
   const [email, setEmail] = useState("");
   const [position, setPosition] = useState("");
 
-  const addEmployee = ()=>{
-    axios.post('http://localhost:3001/create', {
+  const addEmployee = () => {
+    console.log(name)
+    Axios.post('http://localhost:3001/create', {
       name: name,
       age: age,
       email: email,
       position: position
-    });
+    }).then(()=>{
+      console.log("Done");
+    })
   }
 
 
@@ -28,19 +31,19 @@ function App() {
             setName(event.target.value);
           }}
         />
-        <lable>Age:</lable>
+        <label>Age:</label>
         <input type="number" 
         onChange={(event) => {
           setAge(event.target.value);
         }}
         />
-        <lable>Email:</lable>
+        <label>Email:</label>
         <input type="text" 
         onChange={(event) => {
           setEmail(event.target.value);
         }}
         />
-        <lable>Position:</lable>
+        <label>Position:</label>
         <input type="text" 
         onChange={(event) => {
           setPosition(event.target.value);
