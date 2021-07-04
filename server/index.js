fb9d8fa82f2b382a17cdf26457842f85b0f561fd
Minakrv <mnkrv@yahoo.com>
2021-07-04 03:23:21 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+
+app.use(cors());
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
