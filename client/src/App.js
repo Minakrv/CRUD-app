1badb18ca3745f9fbfa5a73fd64764e019182043
Minakrv <mnkrv@yahoo.com>
2021-07-05 16:45:39 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,24 +10,21 @@ function App() {
   const [employeeList, setEmployeeList] = useState([]);
 
   const addEmployee = () => {
-    
-    Axios.post('http://localhost:3001/create', {
+    Axios.post("http://localhost:3001/create", {
       name: name,
       age: age,
       email: email,
-      position: position
-    }).then(()=>{
+      position: position,
+    }).then(() => {
       console.log("Success");
-    })
-  }
+    });
+  };
 
   const getEmployee = () => {
-    Axios.get('http://localhost:3001/employees', ).then((response)=>{
-      setEmployeeList(response.data)
-      
-    })
-  }
-
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
 
   return (
     <div className="App">
@@ -40,31 +37,34 @@ function App() {
           }}
         />
         <label>Age:</label>
-        <input type="number" 
-        onChange={(event) => {
-          setAge(event.target.value);
-        }}
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
         />
         <label>Email:</label>
-        <input type="text" 
-        onChange={(event) => {
-          setEmail(event.target.value);
-        }}
+        <input
+          type="text"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
         />
         <label>Position:</label>
-        <input type="text" 
-        onChange={(event) => {
-          setPosition(event.target.value);
-        }}
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
         />
-        <button onClick = {addEmployee}> Add Employee</button>
+        <button onClick={addEmployee}> Add Employee</button>
       </div>
       <br />
       <div className="employee">
         <hr />
         <button onClick={getEmployee}>Show Employees</button>
-        {employeeList.map((val,key)=>{
-          return <div className = "employ"> {val.name}</div>
+        {employeeList.map((val, key) => {
+          return <div className="employ"> {val.name}</div>;
         })}
       </div>
     </div>
